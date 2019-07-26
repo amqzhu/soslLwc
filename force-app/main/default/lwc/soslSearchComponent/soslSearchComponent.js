28459585b38fe0e59e1a579314e29020f8169bc4
Aubrianna Zhu <aubrianna.zhu@propelplm.com>
2019-07-26 15:32:32 -0700

    datatable with row actions

--- force-app/main/default/lwc/soslSearchComponent/soslSearchComponent.js
+++ force-app/main/default/lwc/soslSearchComponent/soslSearchComponent.js
@@ -9,12 +9,28 @@ const columns = [
     { label: 'CV TITLE', fieldName: 'Title' },
 ];
 
+const actions = [
+    { label: 'Go to Item', name: 'go_to_item' },
+    { label: 'Go to File', name: 'go_to_file' },
+];
+
+const columnsAction = [
+    { label: 'CV ID', fieldName: 'Id' },
+    { label: 'CV TITLE', fieldName: 'Title' },
+    { label: 'VersionData', fieldName: 'VersionData' },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions },
+    }
+];
+
 export default class LightningExampleInputSearch extends LightningElement {
     @track searchKey;
     @track searchKeySOSL;
     @track cvSOSL;
 
     @track columns = columns;
+    @track columnsAction = columnsAction;
     @track data;
     @track error;
 
@@ -25,7 +41,7 @@ export default class LightningExampleInputSearch extends LightningElement {
     @wire(getContentVersions, { cvSOSL: '$cvSOSL' }) cvsTable({ error, data }) {
         if (data) {
             this.data = data;
-            console.log(this.data) //eslint-disable-line
+            console.log('data', this.data) //eslint-disable-line
         } else if (error) {
             this.error = error;
         }
@@ -63,5 +79,53 @@ export default class LightningExampleInputSearch extends LightningElement {
         // });
         // // 3. Fire the custom event
         // this.dispatchEvent(selectEvent);
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        console.log(event.detail); //eslint-disable-line
+        switch (actionName) {
+            case 'go_to_item':
+                this.goToItem(row);
+                break;
+            case 'go_to_file':
+                this.goToFile(row);
+                break;
+            default:
+        }
+    }
+
+    goToItem(row) {
+        const { Id } = row;
+        const index = this.findRowIndexById(Id);
+        console.log(row, Id, index) // eslint-disable-line
+    }
+
+    findRowIndexById(id) {
+        let ret = -1;
+        this.data.some((row, index) => {
+            if (row.Id === id) {
+                ret = index;
+                return true;
+            }
+            return false;
+        });
+        return ret;
+    }
+
+
+//     gotoURL: function (component, event, helper) {
+//     var urlEvent = $A.get("e.force:navigateToURL");
+//     urlEvent.setParams({
+//         "url": "/006/o"
+//     });
+//     urlEvent.fire();
+// }
+    goToFile(row) {
+        const { Id } = row;
+        let url = 'https://curious-hawk-12nndm-dev-ed.lightning.force.com/lightning/r/ContentDocument/'+ Id + '/view'
+        console.log('url:'+url)// eslint-disable-line
+
     }
 }
